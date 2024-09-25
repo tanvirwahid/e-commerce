d8bfbaad3c511e22b28b0c8e77c6c4fafc606ee5
Tanvir <nevillongbottom33@gmail.com>
2024-09-25 23:19:45 +0600

    Add api version

--- routes/api.php
+++ routes/api.php
@@ -5,29 +5,35 @@ use App\Http\Controllers\Apis\OrderController;
 use App\Http\Controllers\Apis\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::get('/products', [ProductController::class, 'index']);
-
 Route::group([
-    'middleware' => 'jwt.auth',
+    'prefix' => config('api.version').'/'
 ], function () {
-    Route::group([
-        'middleware' => 'role:admin',
-        'prefix' => 'products',
-    ], function () {
-        Route::post('', [ProductController::class, 'store']);
-        Route::put('/{product}', [ProductController::class, 'update']);
-    });
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('products', [ProductController::class, 'index']);
 
     Route::group([
-        'middleware' => 'role:user',
-        'prefix' => 'orders',
+        'middleware' => 'jwt.auth',
     ], function () {
-        Route::get('', [OrderController::class, 'index']);
-        Route::post('', [OrderController::class, 'store']);
+        Route::group([
+            'middleware' => 'role:admin',
+            'prefix' => 'products',
+        ], function () {
+            Route::post('', [ProductController::class, 'store']);
+            Route::put('/{product}', [ProductController::class, 'update']);
+        });
+
+        Route::group([
+            'middleware' => 'role:user',
+            'prefix' => 'orders',
+        ], function () {
+            Route::get('', [OrderController::class, 'index']);
+            Route::post('', [OrderController::class, 'store']);
+        });
+
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('logout', [AuthController::class, 'logout']);
     });
 
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('logout', [AuthController::class, 'logout']);
 });
+
